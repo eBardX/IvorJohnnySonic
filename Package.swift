9545bc2de8b26c5b8180b30fc2fc5cfabe835480
John Pusey <ebardx@gmail.com>
2026-05-12 14:43:52 -0400

    Add support for SwiftFormat

--- Package.swift
+++ Package.swift
@@ -4,22 +4,6 @@
 
 import PackageDescription
 
-let package = Package(name: "IvorJohnnySonic",
-                      platforms: [.iOS(.v18),
-                                  .macOS(.v15)],
-                      products: [.library(name: "IvorJohnnySonic",
-                                          targets: ["IvorJohnnySonic"])],
-                      dependencies: [.package(url: "https://github.com/swiftlang/swift-docc-plugin.git",
-                                              .upToNextMajor(from: "1.1.0")),
-                                     .package(url: "https://github.com/eBardX/XestiTools.git",
-                                              .upToNextMajor(from: "7.2.0"))],
-                      targets: [.target(name: "IvorJohnnySonic",
-                                        dependencies: [.product(name: "XestiTools",
-                                                                package: "XestiTools")]),
-                                .testTarget(name: "IvorJohnnySonicTests",
-                                            dependencies: [.target(name: "IvorJohnnySonic")])],
-                      swiftLanguageModes: [.v6])
-
 let swiftSettings: [SwiftSetting] = [.defaultIsolation(nil),
                                      .enableUpcomingFeature("ExistentialAny"),
                                      .enableUpcomingFeature("ImmutableWeakCaptures"),
@@ -28,10 +12,18 @@ let swiftSettings: [SwiftSetting] = [.defaultIsolation(nil),
                                      .enableUpcomingFeature("MemberImportVisibility"),
                                      .enableUpcomingFeature("NonisolatedNonsendingByDefault")]
 
-for target in package.targets {
-    var settings = target.swiftSettings ?? []
-
-    settings.append(contentsOf: swiftSettings)
-
-    target.swiftSettings = settings
-}
+let package = Package(name: "IvorJohnnySonic",
+                      platforms: [.iOS(.v18),
+                                  .macOS(.v15)],
+                      products: [.library(name: "IvorJohnnySonic",
+                                          targets: ["IvorJohnnySonic"])],
+                      dependencies: [.package(url: "https://github.com/eBardX/XestiTools.git",
+                                              .upToNextMajor(from: "7.2.0"))],
+                      targets: [.target(name: "IvorJohnnySonic",
+                                        dependencies: [.product(name: "XestiTools",
+                                                                package: "XestiTools")],
+                                        swiftSettings: swiftSettings),
+                                .testTarget(name: "IvorJohnnySonicTests",
+                                            dependencies: [.target(name: "IvorJohnnySonic")],
+                                            swiftSettings: swiftSettings)],
+                      swiftLanguageModes: [.v6])
